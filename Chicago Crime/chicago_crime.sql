ec890c0ffaf730d96ce6a9dd6b4c28981e1b4af3
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-03 13:48:02 +0700

    Chicago Crime

--- Chicago Crime/chicago_crime.sql
+++ Chicago Crime/chicago_crime.sql
@@ -5,25 +5,25 @@ SELECT * FROM practice..chicago_crime
 SELECT primary_type, SUM(beat) AS total_beat, SUM(ward) AS total_ward
 FROM practice..chicago_crime
 GROUP BY primary_type
-ORDER BY primary_type
+ORDER BY primary_type ASC
 
 -- Looking highest of beat vs ward based on primary type and description
 SELECT primary_type, description, MAX(beat) AS max_beat, MAX(ward) AS max_ward
 FROM practice..chicago_crime
 GROUP BY primary_type, description
-ORDER BY primary_type
+ORDER BY primary_type ASC
 
 -- Looking highest community area based on primary type over the year equals 2010
 SELECT primary_type, year, MAX(community_area) AS max_community_area
 FROM practice..chicago_crime
 WHERE year >= '2010'
 GROUP BY primary_type, year
-ORDER BY primary_type
+ORDER BY primary_type ASC
 
 -- Looking total community area based on year and arrest
 SELECT year, arrest, SUM(community_area) AS total_community_area
 FROM practice..chicago_crime
-GROUP BY year, arrest
+GROUP BY year, arrest ASC
 
 -- Looking total community area based on location description
 SELECT location_description, SUM(community_area) AS total_community_area
@@ -34,12 +34,34 @@ GROUP BY location_description
 SELECT primary_type, arrest, SUM(beat) AS total_beat
 FROM practice..chicago_crime
 GROUP BY primary_type, arrest
-ORDER BY primary_type
+ORDER BY primary_type ASC
 
 -- Looking total ward based on description and arrest
 SELECT description, arrest, SUM(ward) AS total_ward
 FROM practice..chicago_crime
 GROUP BY description, arrest
-ORDER BY description
+ORDER BY description ASC
 
+-- Looking total beat vs total community area which total beat has more than equals to 1000 based on primary type, description and location description
+SELECT primary_type, description, location_description, SUM(beat) AS total_beat, SUM(community_area) AS total_community_area 
+FROM practice..chicago_crime
+GROUP BY primary_type, description, location_description
+HAVING SUM(beat) >= 1000
+ORDER BY primary_type ASC
 
+-- Looking community area based on primary type, description, location description and community area
+SELECT primary_type, description, location_description, community_area,
+CASE 
+	WHEN community_area > 50 THEN 'Very Large'
+	WHEN community_area = 50 THEN 'Large'
+	ELSE 'Very Small'
+END AS community_area_description
+FROM practice..chicago_crime
+ORDER BY primary_type ASC
+
+-- Looking total beat, ward and community area based on primary type, description and location description on theft, assault and battery
+SELECT primary_type, description, location_description, SUM(beat) AS total_beat, SUM(ward) AS total_ward, SUM(community_area) AS total_community_area
+FROM practice..chicago_crime
+WHERE primary_type IN ('THEFT', 'ASSAULT', 'BATTERY')
+GROUP BY primary_type, description, location_description
+ORDER BY primary_type ASC
